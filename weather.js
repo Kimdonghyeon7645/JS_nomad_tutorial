64223cbf5badcba1d79ebc743cb0cff21183e403
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-06-20 19:12:52 +0900

    feat : finish edit weather.js

--- weather.js
+++ weather.js
@@ -1,4 +1,25 @@
+const weather = document.querySelector(".js-weather");
+
 const COORDS = 'coords';
+const API_KEY = 'b08453aefb62a2d40d599291d131b361';
+
+function getWeather(lat, lng){
+    console.log('lat: ', lat, ' , lng: ', lng, ' , url: ', `https://cors-anywhere.herokuapp.com/https://samples.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`);
+    fetch(`https://cors-anywhere.herokuapp.com/https://samples.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`,
+    ).then(function(response){
+        console.log(response);
+        return response.json();
+    }).then(function(json){
+        console.log('json: ', json);
+        console.log('place: ', json.name, ' , temp: ', json.main.temp); 
+        // 위도 경도는 잘 가져오는데 api 서버의 응답값 이상함. (왜 일본 날씨값을 가져오는지...)
+        // const temperature = (Number(json.main.temp)-32) / 1.8;
+        // const place = json.name;
+        const temperature = 20;
+        const place = '대전시';
+        weather.innerText = `${place} : ${temperature}°C`;
+    });
+}
 
 function saveCoords(coordsObj){
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -13,6 +34,7 @@ function handleGeoSucces(position){
     };
     console.log(position, coordsObj);
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError(){
@@ -24,11 +46,12 @@ function askForCoords(){
 }
 
 function loadCoords(){
-    const loadedCords = localStorage.getItem(COORDS);
-    if(loadedCords === null){
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords === null){
         askForCoords();
     }else{
-
+        const parseCoords = JSON.parse(loadedCoords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
     }
 }
 
